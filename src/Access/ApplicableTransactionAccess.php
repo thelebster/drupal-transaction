ca749cd6f162195b2c97fb12d9bd61c982c26b1e
adan <adan@516420.no-reply.drupal.org>
2020-03-29 13:19:51 +0200

    Issue #3065541 by corneboele, manuel.adan, jungle: Transaction entity is not always an entity in ApplicableTransactionAccess

--- src/Access/ApplicableTransactionAccess.php
+++ src/Access/ApplicableTransactionAccess.php
@@ -5,9 +5,9 @@ namespace Drupal\transaction\Access;
 use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Entity\ContentEntityInterface;
 use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Routing\Access\AccessInterface;
 use Drupal\Core\Routing\RouteMatchInterface;
-use Drupal\transaction\Entity\TransactionType;
 use Drupal\transaction\TransactionTypeInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
@@ -33,35 +33,46 @@ class ApplicableTransactionAccess implements AccessInterface {
   protected $requestStack;
 
   /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
    * ApplicableTransactionAccess constructor.
    *
    * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
    *   The current route match.
    * @param \Symfony\Component\HttpFoundation\RequestStack $request_stack
    *   The request stack.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
    */
-  public function __construct(RouteMatchInterface $route_match, RequestStack $request_stack) {
+  public function __construct(RouteMatchInterface $route_match, RequestStack $request_stack, EntityTypeManagerInterface $entity_type_manager) {
     $this->currentRouteMatch = $route_match;
     $this->requestStack = $request_stack;
+    $this->entityTypeManager = $entity_type_manager;
   }
 
   /**
    * Check if the transaction type is applicable to the content entity.
    *
-   * @param \Drupal\Core\Entity\ContentEntityInterface $entity
-   *   The involved content entity.
-   * @param \Drupal\transaction\TransactionTypeInterface $transaction_type
-   *   The transaction type.
-   * @param \Symfony\Component\Routing\Route $route
-   *   The route to check access for.
-   * @param \Symfony\Component\HttpFoundation\Request $request
-   *   The current request.
+   * @param \Drupal\Core\Entity\ContentEntityInterface|null $entity
+   *   (optional) The involved content entity, determined from request if NULL.
+   * @param \Drupal\transaction\TransactionTypeInterface|null $transaction_type
+   *   (optional) The transaction type, determined from the request or the route
+   *   options if NULL.
+   * @param \Symfony\Component\Routing\Route|null $route
+   *   (optional) The route to check access for.
+   * @param \Symfony\Component\HttpFoundation\Request|null $request
+   *   (optional) The current request.
    *
    * @return \Drupal\Core\Access\AccessResultInterface
    *   Allowed if the transaction type is applicable to the content entity.
    */
   public function access(ContentEntityInterface $entity = NULL, TransactionTypeInterface $transaction_type = NULL, Route $route = NULL, Request $request = NULL) {
-    // Check access for the current route if none given.
+    // Check access for the current route if none was given.
     if (!$route) {
       $route = $this->currentRouteMatch->getRouteObject();
     }
@@ -69,43 +80,17 @@ class ApplicableTransactionAccess implements AccessInterface {
       $request = $this->requestStack->getCurrentRequest();
     }
 
-    // Look after the target entity.
-    if (!$entity) {
-      $entity = $request->get('target_entity');
-    }
-
-    if (!$entity
-      || !is_object($entity)
-      || !($entity instanceof ContentEntityInterface)) {
-      $entity = NULL;
-
-      if (!($target_entity_type_id = $route->getOption('_transaction_target_entity_type_id'))
-        && ($target_entity_type = $request->get('target_entity_type'))
-        && is_object($target_entity_type)
-        && $target_entity_type instanceof EntityTypeInterface) {
-        $target_entity_type_id = $target_entity_type->id();
-      }
-
-      if ($target_entity_type_id) {
-        $entity = $request->get($target_entity_type_id);
-      }
-    }
-
-    // Look after the transaction type.
+    // Try to determine the transaction type if it was not given.
+    $transaction_type = $transaction_type ? $transaction_type : $this->guessTransactionType($route, $request);
     if (!$transaction_type) {
-      $transaction_type = $request->get('transaction_type');
+      // Unable to determine the transaction type.
+      return AccessResult::forbidden();
     }
 
-    if (!$transaction_type
-      || !is_object($transaction_type)
-      || !($transaction_type instanceof TransactionTypeInterface)) {
-      $transaction_type = NULL;
-      if ($transaction_type_id = $route->getOption('_transaction_transaction_type_id')) {
-        $transaction_type = TransactionType::load($transaction_type_id);
-      }
-    }
-
-    if (!$entity || !$transaction_type) {
+    // Try to determine the target entity.
+    $entity = $entity ? $entity : $this->guessTargetEntity($route, $request);
+    if (!$entity) {
+      // Unable to determine the target entity.
       return AccessResult::forbidden();
     }
 
@@ -116,6 +101,74 @@ class ApplicableTransactionAccess implements AccessInterface {
     return $result
       ->addCacheableDependency($entity)
       ->addCacheableDependency($transaction_type);
+  }
+
+  /**
+   * Tries to determine the transaction type from request and route.
+   *
+   * @param \Symfony\Component\Routing\Route $route
+   *   The route to check access for.
+   * @param \Symfony\Component\HttpFoundation\Request $request
+   *   The current request.
+   *
+   * @return \Drupal\transaction\TransactionTypeInterface|null
+   *   The transaction type, NULL if cannot be determined.
+   */
+  protected function guessTransactionType(Route $route, Request $request) {
+    $transaction_type = $request->get('transaction_type');
+
+    if (!$transaction_type instanceof TransactionTypeInterface) {
+      $transaction_type_id = is_string($transaction_type)
+        ? $transaction_type
+        : $route->getOption('_transaction_transaction_type_id');
+      if (!empty($transaction_type_id)) {
+        $transaction_type = $this->entityTypeManager
+          ->getStorage('transaction_type')
+          ->load($transaction_type_id);
+      }
+    }
+
+    return $transaction_type;
+  }
+
+  /**
+   * Tries to determine the target entity type from request and route.
+   *
+   * @param \Symfony\Component\Routing\Route $route
+   *   The route to check access for.
+   * @param \Symfony\Component\HttpFoundation\Request $request
+   *   The current request.
+   *
+   * @return \Drupal\Core\Entity\ContentEntityInterface|null
+   *   The target entity type, NULL if cannot be determined.
+   */
+  protected function guessTargetEntity(Route $route, Request $request) {
+    // First try from request argument.
+    $entity = $request->get('target_entity');
+
+    if (!$entity instanceof ContentEntityInterface) {
+      // Try from request argument named as the target entity type.
+      $target_entity_type = $request->get('target_entity_type');
+
+      if ($target_entity_type instanceof EntityTypeInterface) {
+        $target_entity_type_id = $target_entity_type->id();
+      }
+      else {
+        $target_entity_type_id = is_string($target_entity_type)
+          ? $target_entity_type
+          : $route->getOption('_transaction_target_entity_type_id');
+      }
+
+      if ($target_entity_type_id) {
+        $entity = $request->get($target_entity_type_id) ?: $entity;
+        if (is_numeric($entity)) {
+          $entity = $this->entityTypeManager->getStorage($target_entity_type_id)
+            ->load($entity);
+        }
+      }
+    }
+
+    return $entity instanceof ContentEntityInterface ? $entity : NULL;
   }
 
 }
