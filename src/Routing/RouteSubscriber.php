362a180c47771043e0c8137f34c075def0421dfe
Anton Lebedev <mail@lebster.me>
2023-11-27 21:13:39 +0100

    Fix declaration of getSubscribedEvents().

--- src/Routing/RouteSubscriber.php
+++ src/Routing/RouteSubscriber.php
@@ -86,7 +86,7 @@ class RouteSubscriber extends RouteSubscriberBase {
   /**
    * {@inheritdoc}
    */
-  public static function getSubscribedEvents() {
+  public static function getSubscribedEvents(): array {
     $events = parent::getSubscribedEvents();
     $events[RoutingEvents::ALTER] = ['onAlterRoutes', 100];
     return $events;
